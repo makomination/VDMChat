c56f5e4f7d7c4e773eb50f25bf29a6f15bd61144
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-06-15 08:50:27 -0700

    * Some ChatBoardViewController Coding(import FIrebaseDatabase, inherit delegate, protocol for table ... * Added SettingViewCOntroller * Define segway between them

--- VDMChat/ChatBoardViewController.swift
+++ VDMChat/ChatBoardViewController.swift
@@ -7,8 +7,14 @@
 //
 import UIKit
 import Foundation
+import FirebaseDatabase
 
-class ChatBoardViewController: UIViewController {
+class ChatBoardViewController: UIViewController, UITextViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var textView: UITextView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("test")
@@ -16,5 +22,16 @@ class ChatBoardViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //Required for UITableVIewDataSource protocol
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell: UITableViewCell? = nil
+        return tableViewCell!
+    }
+    
+    //Required for UITableVIewDataSource protocol
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 }
---
+++ VDMChat/SettingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.swift
+//  VDMChat
+//
+//  Created by Makoto Ishihara on 2017-06-15.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingViewController: UIViewController, UITextFieldDelegate {
+
+}
